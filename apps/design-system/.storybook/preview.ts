ec2964eb164e42d217b6a019198645dd80535360
R.Moroizumi <m.ryousuke0401@iclioud.com>
2025-09-09 10:34:07 +0900

    ✨ feat: design-system (storybook)構築

---
+++ apps/design-system/.storybook/preview.ts
@@ -0,0 +1,16 @@
+import '@/styles/global.css'
+
+import type { Preview } from '@storybook/nextjs'
+
+const preview: Preview = {
+  parameters: {
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
+    },
+  },
+}
+
+export default preview
---
+++ apps/design-system/stories/button.stories.tsx
@@ -0,0 +1,18 @@
+import type { Meta, StoryObj } from '@storybook/nextjs'
+import { Button } from '@team-up/ui/components/button'
+
+const meta = {
+  title: 'Design System/UI/Button',
+  component: Button,
+  tags: ['autodocs'],
+} satisfies Meta<typeof Button>
+
+export default meta
+
+type Story = StoryObj<typeof meta>
+
+export const Default: Story = {
+  args: {
+    children: 'Button',
+  },
+}
