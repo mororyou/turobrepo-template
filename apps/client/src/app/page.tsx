83d14c9e2189b271ae9b2d86ca78d440ad93d2d6
R.Moroizumi <m.ryousuke0401@iclioud.com>
2025-12-02 04:31:47 +0900

    feat: integrate schemas package into client and hono-api, utilizing sample schema for data validation

--- apps/client/src/app/page.tsx
+++ apps/client/src/app/page.tsx
@@ -1,9 +1,16 @@
+import { sampleSchema } from '@repo/schemas'
 import { Button } from '@repo/ui/components/button'
 
 export default function Home() {
+  const sample = sampleSchema.parse({
+    name: 'John Doe',
+    age: 20,
+  })
+
   return (
     <div className="grid min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 p-8 pb-20 font-sans sm:p-20">
       <Button>Click me</Button>
+      <pre>{JSON.stringify(sample, null, 2)}</pre>
     </div>
   )
 }
--- services/hono-api/src/index.ts
+++ services/hono-api/src/index.ts
@@ -1,9 +1,18 @@
+import { sampleSchema } from '@repo/schemas'
 import { Hono } from 'hono'
 
 const app = new Hono()
 
 app.get('/', (c) => {
-  return c.text('Hello Hono!')
+  const sample = sampleSchema.parse({
+    name: 'John Doe',
+    age: 20,
+  })
+
+  return c.json({
+    message: 'Hello Hono!',
+    sample,
+  })
 })
 
 export default app
